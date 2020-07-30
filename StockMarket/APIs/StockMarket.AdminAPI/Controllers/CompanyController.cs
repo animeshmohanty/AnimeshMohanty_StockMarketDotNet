589a4dbe1fd6fb3d6f75ecf7c59ae8958a041141
Animesh Mohanty <35626679+animeshmohanty@users.noreply.github.com>
2020-07-31 03:06:48 +0530

    Add files via upload

---
+++ StockMarket/APIs/StockMarket.AdminAPI/Controllers/CompanyController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StockMarket.AdminAPI.Services;
+namespace StockMarket.AdminAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+        CompanyService service = new CompanyService();
+        [HttpGet]
+        [Route("GetCompanies")]
+        public IActionResult Get()
+        {
+            return Ok(service.GetCompanies());
+        }
+    }
+}
