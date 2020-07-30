a8a3495d8cac6dfc44090ab33c99aafa328e5a44
Animesh Mohanty <35626679+animeshmohanty@users.noreply.github.com>
2020-07-31 03:11:49 +0530

    Add files via upload

---
+++ StockMarket/APIs/StockMarket.ExcelAPI/Controllers/UploadController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StockMarket.ExcelAPI.Services;
+namespace StockMarket.ExcelAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        UploadService service = new UploadService();
+        [HttpGet]
+        [Route("LoadData")]
+        public IActionResult LoadExcel(string file_name)
+        {
+           try
+            {
+                string path = @"D:\Upload\"+file_name;
+                service.LoadData(path);
+                return Ok("Records Uploaded");
+
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, "Internal Server Error");
+            }
+           
+        }
+    }
+}
---
+++ StockMarket/APIs/StockMarket.ExcelAPI/Models/StockMarketDBContext.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace StockMarket.ExcelAPI.Models
+{
+    public partial class StockMarketDBContext : DbContext
+    {
+        public StockMarketDBContext()
+        {
+        }
+
+        public StockMarketDBContext(DbContextOptions<StockMarketDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Company> Company { get; set; }
+        public virtual DbSet<StockPrice> StockPrice { get; set; }
+        public virtual DbSet<StockUser> StockUser { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=LAPTOP-L9952A4F\\SQLEXPRESS\\MSSQLSERVER2018;Database=StockMarketDB;User ID=sa;Password=pass@word1");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Company>(entity =>
+            {
+                entity.HasKey(e => e.Companyname)
+                    .HasName("PK__company__838B6B7A54181764");
+
+                entity.ToTable("company");
+
+                entity.Property(e => e.Companyname)
+                    .HasColumnName("companyname")
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Ceo)
+                    .HasColumnName("ceo")
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Stockexchage)
+                    .HasColumnName("stockexchage")
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Turnover).HasColumnName("turnover");
+            });
+
+            modelBuilder.Entity<StockPrice>(entity =>
+            {
+                entity.HasKey(e => e.RowId)
+                    .HasName("PK__StockPri__FFEE74519E745899");
+
+                entity.Property(e => e.RowId).HasColumnName("RowID");
+
+                entity.Property(e => e.CurrentPrice)
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Date)
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.StockExchange)
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Time)
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<StockUser>(entity =>
+            {
+                entity.ToTable("stock_user");
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("id")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.Pwd)
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Uname)
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
