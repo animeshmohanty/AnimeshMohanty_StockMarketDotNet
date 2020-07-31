b3d7ddb2697eed928d3fd025450a87adfca6971b
Animesh Mohanty <35626679+animeshmohanty@users.noreply.github.com>
2020-07-31 09:31:56 +0530

    Update StockMarketDBContext.cs

--- StockMarket/EPPlusCore/Models/StockMarketDBContext.cs
+++ StockMarket/EPPlusCore/Models/StockMarketDBContext.cs
@@ -15,7 +15,7 @@ namespace EPPlusCore.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer(@"Server=SANTU\MSSQLSERVER2019;Database=StockMarketDB;User ID=sa;Password=pass@word1");
+                optionsBuilder.UseSqlServer("Server=LAPTOP-L9952A4F\SQLEXPRESS\;Database=StockMarketDB;User ID=sa;Password=pass@word1");
             }
         }
 
