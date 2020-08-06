7236d6ad8d5450a533dd483a34420010f0d6ab68
Animesh Mohanty <35626679+animeshmohanty@users.noreply.github.com>
2020-08-06 12:35:00 +0530

    All Final Changes

--- StockMarket/Frontend/CreateCompany/ngValidation/karma.conf.js
+++ StockMarket/Frontend/CreateCompany/ngValidation/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/ngValidation'),
+      dir: require('path').join(__dirname, './coverage/StockMarket'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
